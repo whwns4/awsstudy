c2b061d731a0b7d85843360556687831a67a24aa
LOKILAB <whwns5@datasolution.kr>
2024-02-05 14:36:51 +0900

    테스트 주석

--- awsstudy/src/test/java/awsstudy/ds/kr/springboot/web/HelloControllerTest.java
+++ awsstudy/src/test/java/awsstudy/ds/kr/springboot/web/HelloControllerTest.java
@@ -1,46 +1,46 @@
 package awsstudy.ds.kr.springboot.web;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.hamcrest.Matchers.is;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.hamcrest.Matchers.is;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
-    @Autowired
-    private MockMvc mvc;
+    //@Autowired
+    //private MockMvc mvc;
 
-    @Test
-    void hello가_리턴된다() throws Exception {
-        //given
-        String hello = "hello";
-
-        //when
-        mvc.perform(get("/hello"))
-                //then
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
-    }
-
-    @Test
-    void helloDto가_리턴된다() throws Exception {
-        String name = "hello";
-        int amount = 1000;
-
-        mvc.perform(
-                        get("/hello/dto")
-                                .param("name", name)
-                                .param("amount", String.valueOf(amount)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
-                .andExpect(jsonPath("$.amount", is(amount))
-                );
-
-    }
+//    @Test
+//    void hello가_리턴된다() throws Exception {
+//        //given
+//        String hello = "hello";
+//
+//        //when
+//        mvc.perform(get("/hello"))
+//                //then
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(hello));
+//    }
+//
+//    @Test
+//    void helloDto가_리턴된다() throws Exception {
+//        String name = "hello";
+//        int amount = 1000;
+//
+//        mvc.perform(
+//                        get("/hello/dto")
+//                                .param("name", name)
+//                                .param("amount", String.valueOf(amount)))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.name", is(name)))
+//                .andExpect(jsonPath("$.amount", is(amount))
+//                );
+//
+//    }
 }
--- awsstudy/src/test/java/awsstudy/ds/kr/springboot/web/IndexControllerTest.java
+++ awsstudy/src/test/java/awsstudy/ds/kr/springboot/web/IndexControllerTest.java
@@ -1,25 +1,25 @@
 package awsstudy.ds.kr.springboot.web;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    //@Autowired
+    //private TestRestTemplate restTemplate;
 
-    @Test
-    void 메인페이지_로딩() {
-        //when
-        String body = this.restTemplate.getForObject("/", String.class);
-
-        //then
-        //assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
-    }
+//    @Test
+//    void 메인페이지_로딩() {
+//        //when
+//        String body = this.restTemplate.getForObject("/", String.class);
+//
+//        //then
+//        //assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+//        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
+//    }
 }
